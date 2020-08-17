4c44c8885ab97bb2910b386b79676e23faa4c31a
Florian Guitton <florian@secretarium.com>
2020-08-17 23:30:05 +0000

    Review the footer content and include contact form

--- packages/www/components/footer.tsx
+++ packages/www/components/footer.tsx
@@ -1,4 +1,5 @@
-import Container from './container'
+import Container from './container';
+import ContactForm from './contact-form';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
 import logo from '../public/assets/images/logoOutlineBlack.svg';
 import { faTwitter, faLinkedinIn, faGithub, faDiscord } from '@fortawesome/free-brands-svg-icons'
@@ -8,7 +9,7 @@ const Footer = () => {
     return (
         <footer className="bg-accent-1 border-t border-accent-2">
             <Container>
-                <div className="py-28 flex flex-col lg:flex-row items-center">
+                <div className="py-28 flex flex-col lg:flex-row items-top">
                     <div className="leading-tight text-xl mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
                         <Link href="/">
                             <a title="Secretarium">
@@ -16,9 +17,16 @@ const Footer = () => {
                             </a>
                         </Link>
                         <div className="text-center text-lg lg:text-left mb-4 text-gray-700 pb-5">
-                            Europe:<br />
-                            1 Bank St<br />
+                            Société Générale<br />
+                            Innovation Center, 5th floor<br />
+                            1 Bank Street<br />
                             London E14 4SG<br />
+                            United Kingdom<br />
+                            <br />
+                            Allen &amp; Overy<br />
+                            FUSE incubator, 1st floor<br />
+                            One Bishops Square, Spitalfields<br />
+                            London E1 6AD<br />
                             United Kingdom<br />
                             <a href="mailto:contact@secretarium.com">contact@secretarium.com</a>
                         </div>
@@ -30,19 +38,7 @@ const Footer = () => {
                         </div>
                     </div>
                     <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-                        {/*  <a
-                            href="https://nextjs.org/docs/basic-features/pages"
-                            className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-                        >
-                            Read Documentation
-            </a>
-                        <a
-                            href={`https://github.com/secretarium`}
-                            target='_blank'
-                            className="mx-3 font-bold hover:underline"
-                        >
-                            View on GitHub
-            </a> */}
+                        <ContactForm />
                     </div>
                 </div>
                 <div className="w-full text-center text-sm pb-20">
