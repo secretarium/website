534cb70e0a703b579e6f91bda77c9d22cb4a5ca5
ektarathi <rathiekta2801@gmail.com>
2021-03-05 15:40:29 +0000

    Updated Blog Page

--- nx-secretarium-website/apps/nxwebsite/components/Card.tsx
+++ nx-secretarium-website/apps/nxwebsite/components/Card.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Image from './Image';
 
 const Card = ({ article }) => {
+  console.log(article);
   return (
     <figure className="mbg-gray-100 rounded-xl p-3">
+      <h2 className="text-red-700 pb-3 uppercase text-center">{article.title}</h2>
       <Image image={article.image} />
       <div className="pt-6 text-center space-y-4">
         <blockquote>
           <p className="text-lg font-semibold">{article.description}</p>
         </blockquote>
         <figcaption className="font-medium">
-          <div className="text-cyan-600">{article.author.name}</div>
+          <div className="text-cyan-600 text-purple-600">{article.author.name}</div>
         </figcaption>
       </div>
     </figure>
